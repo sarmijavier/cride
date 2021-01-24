0533ea9ba9154b05ee2f70d166c19d641e57bc09
Yahuarcani <mayayahuarcani@gmail.com>
2021-01-23 23:49:34 -0500

    odels

--- cride/users/models.py
+++ cride/users/models.py
@@ -3,6 +3,7 @@
 #Django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 
 #utilities
@@ -16,15 +17,39 @@ class User(CRideModel, AbstractUser):
     to email and add some extra fields.
     """
 
-    email = models.EmailField(_
+    email = models.EmailField(
         'email address',
         unique=True,
-        error_message={
+        error_messages={
             'unique': 'A user with that email already exists.'
         }
     )
 
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message='Phone number must be entered in the format: +999999999. Up to 15 digits allowed'
+    )
     phone_number = models.CharField(
+        validators=[phone_regex],
         max_length=30,
         blank=True
     )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    is_client = models.BooleanField(
+        'client',
+        default=True,
+        help_text=(
+            'Help easily distinguish users and perform queries.'
+            'Clients are the main type of user.'   
+        )
+    )
+
+    is_verified = models.BooleanField(
+        'verified',
+        default=False,
+        help_text='Change to true when the user has verified its email address.'
+    )
+
--- cride/utils/models.py
+++ cride/utils/models.py
@@ -4,28 +4,27 @@
 from django.db import models 
 
 class CRideModel(models.Model):
+
     """ Comparte Ride base model. 
     
     CRideModel acts as an abstract base class from which every
     other model in the project will inherit. This class provides
     every table with the following attributes:
-        + created (DataTime): Store the datetime the object was created
-        + modified (DataTime): Store last datetime the object was modified
-
-    
+    + created (DataTime): Store the datetime the object was created
+    + modified (DataTime): Store last datetime the object was modified
     """
    
-   created = models.DateTimeField(
+    created = models.DateTimeField(
        'created at',
        auto_now_add=True,
        help_text='Date time on which the object was created.'
-   )
+    )
  
-   modified = models.DateTimeField(
+    modified = models.DateTimeField(
        'modified at',
-       auto_now_add=True,
+       auto_now=True,
        help_text='Date time on which the object was last modified.'
-   )
+    )
 
 
     class Meta:
